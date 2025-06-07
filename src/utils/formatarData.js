f41332d764f76a6f88ddf645cdedd18684d582f6
Egon Alves <egon.alves@sercompe.com.br>
2025-06-07 14:08:40 -0300

    Refatorar o codigo

--- src/utils/formatarData.js
+++
@@ -1,8 +0,0 @@
-// utils/formatarData.js
-function formatarData(dateString) {
-    const date = new Date(dateString);
-    return date.toLocaleString('pt-BR', { timeZone: 'UTC' });
-  }
-  
-  module.exports = { formatarData };
-  
